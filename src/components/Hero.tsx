d955c281112ce9b3ac234b5f30d1de6f9a56b8ca
Bill Ntwali <billntwali996@gmail.com>
2026-07-22 23:21:56 -0400

    Update hero resume link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -196,7 +196,7 @@ export default function Hero() {
             About Me
           </button>
           <a
-            href="https://drive.google.com/file/d/1iZOJ03VMnImzHyX0ZlexTig9RbPHtQv-/view?usp=sharing"
+            href="https://drive.google.com/file/d/1xUIb2kmS36AEJmLOdTZo9lprXHaJ24hi/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="btn-ghost"
